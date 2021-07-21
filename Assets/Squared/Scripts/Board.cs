bd2ee79eb601a461ef7e8c905ba3c483ae78dcf9
Wesley Matthews <81110199+wdmatthews@users.noreply.github.com>
2021-07-21 12:54:43 -0500

    Prevented slide cancelling, removed old sliding, and added a base three tile

--- Assets/Squared/Scripts/Board.cs
+++ Assets/Squared/Scripts/Board.cs
@@ -11,6 +11,7 @@ namespace Squared
         [SerializeField] private SpriteRenderer _renderer = null;
         [SerializeField] private Vector2Int _boardSize = new Vector2Int(4, 4);
         [SerializeField] private TileSO[] _tileSOs = { };
+        [SerializeField] private float _slideCooldown = 1;
         #endregion
 
         #region Runtime Fields
@@ -19,6 +20,7 @@ namespace Squared
         private List<Tile> _tiles = new List<Tile>();
         private Dictionary<Vector2Int, Tile> _tilesByPosition = new Dictionary<Vector2Int, Tile>();
         private Dictionary<TileSO, Stack<Tile>> _inactiveTiles = new Dictionary<TileSO, Stack<Tile>>();
+        private float _slideCooldownTimer = 0;
         #endregion
 
         #region Properties
@@ -39,6 +41,11 @@ namespace Squared
         {
             ReadTileSOs();
             PlaceInitialTiles();
+        }
+
+        private void Update()
+        {
+            _slideCooldownTimer = Mathf.Clamp(_slideCooldownTimer - Time.deltaTime, 0, _slideCooldown);
         }
         #endregion
 
@@ -68,8 +75,8 @@ namespace Squared
             //}
             PlaceTile(_tileSOs[0], new Vector2Int(0, 0));
             PlaceTile(_tileSOs[0], new Vector2Int(1, 1));
-            PlaceTile(_tileSOs[0], new Vector2Int(2, 2));
-            PlaceTile(_tileSOs[0], new Vector2Int(3, 3));
+            PlaceTile(_tileSOs[1], new Vector2Int(2, 2));
+            PlaceTile(_tileSOs[1], new Vector2Int(3, 3));
         }
 
         private Vector3 BoardToWorldPosition(Vector2Int boardPosition)
@@ -158,6 +165,9 @@ namespace Squared
         #region Public Methods
         public void SlideTiles(Vector2Int direction)
         {
+            if (!Mathf.Approximately(_slideCooldownTimer, 0)) return;
+            _slideCooldownTimer = _slideCooldown;
+
             if (direction.x == -1)
             {
                 for (int x = 1; x < _boardSize.x; x++)
@@ -214,74 +224,6 @@ namespace Squared
                     tile.SetPower(tile.NextPower);
                 }
             }
-
-            //List<Tile> movableTiles = new List<Tile>(_tiles);
-            //int slideIteration = 0;
-
-            //for (int i = movableTiles.Count - 1; i >= -1; i--)
-            //{
-            //    if (movableTiles.Count == 1) i = 0;
-            //    Tile tile = movableTiles[i];
-            //    if (slideIteration == 0) tile.NextBoardPosition = tile.BoardPosition + direction;
-
-            //    if (tile.NextBoardPosition.x >= 0 && tile.NextBoardPosition.x < _boardSize.x
-            //        && tile.NextBoardPosition.y >= 0 && tile.NextBoardPosition.y < _boardSize.y)
-            //    {
-            //        if (_tilesByPosition.ContainsKey(tile.NextBoardPosition))
-            //        {
-            //            Tile otherTile = _tilesByPosition[tile.NextBoardPosition];
-            //            movableTiles.RemoveAt(i);
-
-            //            if (tile.Data.BaseNumber == otherTile.Data.BaseNumber && tile.Power == otherTile.Power)
-            //            {
-            //                otherTile.NextPower++;
-            //                tile.Remove();
-            //                _inactiveTiles[tile.Data].Push(tile);
-            //                _tilesByPosition.Remove(tile.BoardPosition);
-            //                _tiles.Remove(tile);
-            //            }
-            //        }
-            //        else
-            //        {
-            //            _tilesByPosition.Remove(tile.BoardPosition);
-            //            tile.BoardPosition = tile.NextBoardPosition;
-            //            _tilesByPosition.Add(tile.BoardPosition, tile);
-            //            tile.NextBoardPosition += direction;
-            //        }
-            //    }
-            //    else
-            //    {
-            //        movableTiles.RemoveAt(i);
-            //    }
-
-            //    if (i == 0)
-            //    {
-            //        slideIteration++;
-            //        int movableCount = movableTiles.Count;
-            //        if (movableCount > 0) i = movableCount - 1;
-            //        else break;
-            //    }
-            //}
-
-            //for (int i = _tiles.Count - 1; i >= 0; i--)
-            //{
-            //    Tile tile = _tiles[i];
-
-            //    if (tile.BoardPosition != tile.NextBoardPosition)
-            //    {
-            //        tile.Move(BoardToWorldPosition(tile.NextBoardPosition), tile.NextBoardPosition);
-            //    }
-
-            //    if (tile.Power != tile.NextPower)
-            //    {
-            //        if (tile.SetPower(tile.NextPower))
-            //        {
-            //            _inactiveTiles[tile.Data].Push(tile);
-            //            _tilesByPosition.Remove(tile.BoardPosition);
-            //            _tiles.RemoveAt(i);
-            //        }
-            //    }
-            //}
         }
         #endregion
     }
